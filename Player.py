92ce69709194c368e8f8a9dac0a8e9574514c02f
AS-line <taibov.0321@mail.ru>
2018-02-27 23:18:53 +0300

    0.2.1 hp\mp

--- Player.py
+++ Player.py
@@ -26,7 +26,7 @@ class Player():
         self.moving = [0, 0, 0, 0]
 
     def move(self):
-        if self.moving[0] == 1:
+        if self.moving[RIGHT] == 1:
             self.x += PLAYER_SPEED
             self.direction = RIGHT
         if self.moving[1] == 1:
@@ -39,11 +39,30 @@ class Player():
 
         if self.x <= 0: self.x = 0
         if self.y <= 0: self.y = 0
-        if self.x >= SCREEN_WIDTH - 22: self.x = SCREEN_WIDTH - 22
-        if self.y >= SCREEN_HEIGHT - 41: self.y = SCREEN_HEIGHT - 41
+        if self.x >= SCREEN_WIDTH - 47: self.x = SCREEN_WIDTH - 47
+
+        if self.y >= SCREEN_HEIGHT - 100: self.y = SCREEN_HEIGHT - 100
 
     def render(self, screen):
         screen.blit(self.images[self.direction][self.state], (self.x, self.y))
 
     def render_ui(self, screen):
-        screen.blit(pygame.image.load("data/hpframe_short.png").convert_alpha(), (self.x, self.y+89))
+        screen.blit(pygame.image.load("data/hpframe_short.png").convert_alpha(), (self.x-10, self.y+89))
+        screen.blit(pygame.image.load("data/mannaframe_short.png").convert_alpha(), (self.x - 10, self.y + 89+7))
+        m = 1
+        z = self.hp // 10
+        while m <= z:
+            screen.blit(pygame.image.load("data/hptick.png"), (self.x-15+m*5, self.y+90))
+            m += 1
+        m = 1
+        z = self.mp // 10
+        while m <= z:
+            screen.blit(pygame.image.load("data/mannatick.png"), (self.x-14+m*5, self.y+97))
+            m += 1
+
+    def tick(self):
+        if self.mp < MAX_MP:
+            self.mp += MP_REG
+        if self.hp < MAX_HP:
+            self.hp += HP_REG
+
